86ce00ec2dc2d11bc6c432dd51a6429ecde5b59e
Abdur Rahman Akbar <abdurrahmanakbar99@gmail.com>
2023-10-29 19:55:48 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import showBanner from "node-banner";
+import gradient from "gradient-string";
+(async () => {
+    await showBanner('Word Counter', 'This app can count the words of any paragraph no matter how long it is!', 'red', 'blue');
+})();
+async function main() {
+    let condition = true;
+    while (condition) {
+        const { enterParagraph } = await inquirer.prompt([{
+                name: "enterParagraph",
+                type: "input",
+                message: gradient.instagram("Please enter the paragraph you want to count words of!")
+            }]);
+        let again = [{
+                name: "tryAgain",
+                type: "confirm",
+                message: gradient.rainbow("Do you want to count words again?")
+            }];
+        let words = enterParagraph.split(" ");
+        console.log(gradient.summer(`The number of words in this paragraph is ${words.length}`));
+        let { tryAgain } = await inquirer.prompt(again);
+        condition = tryAgain;
+        if (!condition) {
+            console.log(chalk.yellow("Thank you for using this!"));
+        }
+    }
+}
+setTimeout(() => {
+    main();
+}, 500);
